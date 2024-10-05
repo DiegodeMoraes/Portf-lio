2943a72a42baff1579bfdc8cc4fa26525825d5a2
Diego Dias <diegodiasdemoraes@gmail.com>
2024-10-05 10:33:50 -0300

    adicionando modo escuro

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
 window.onload = function() {
   alert('Bem-vindo ao meu portfólio!');
 }
+
+// Alternar entre modo claro e escuro
+const toggleButton = document.getElementById('dark-mode-toggle');
+
+toggleButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+
+    if (toggleButton.textContent === 'Modo Escuro') {
+        toggleButton.textContent = 'Modo Claro';
+    } else {
+        toggleButton.textContent = 'Modo Escuro';
+    }
+});
